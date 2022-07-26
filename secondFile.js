c584f62b5d2ba700e60e4ff9720cc43fc56b4517
anuradha360dev <anuradha.barnwal@360degreecloud.in>
2022-07-26 10:48:27 +0530

    new branch

--- secondFile.js
+++ secondFile.js
@@ -1,1 +1,11 @@
-let a = 2 + 4;
+function largest(arr) {
+    let i;
+    let max = arr[0];
+    
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+     
+  return max;
+}
